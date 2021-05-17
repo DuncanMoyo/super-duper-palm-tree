49683d0fd9d44bb87632083a4a98c0623692c3b4
Duncan Moyo <duncanfmoyo@gmail.com>
2021-05-17 15:40:23 +0200

    mapping lists to widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import './question.dart'; 
+import './question.dart';
 import './answer.dart';
 
 void main() => runApp(MyApp());
@@ -15,22 +15,30 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-
   var _questionIndex = 0;
 
   void _answerQuestion() {
     setState(() {
-      _questionIndex = _questionIndex + 1;  
+      _questionIndex = _questionIndex + 1;
     });
     print(_questionIndex);
   }
 
-
   @override
   Widget build(BuildContext context) {
     var questions = [
-      'What\'s your favourite color?',
-      'What\'s your favourite animal?',
+      {
+        'questionText': 'What\'s your favourite color?',
+        'answers': ['Black', 'White', 'Red', 'Blue'],
+      },
+      {
+        'questionText': 'What\'s your favourite animal?',
+        'answers': ['Lion', 'Eagle', 'Cheetah', 'Leopard'],
+      },
+         {
+        'questionText': 'Which Cricket Team is the Best?',
+        'answers': ['Australia', 'England', 'India', 'New Zealand'],
+      },
     ];
     return MaterialApp(
       home: Scaffold(
@@ -40,15 +48,14 @@ class _MyAppState extends State<MyApp> {
         body: Column(
           children: [
             Question(
-              questions[_questionIndex],
+              questions[_questionIndex]['questionText'],
             ),
-            Answer(_answerQuestion),
-            Answer(_answerQuestion),
-            Answer(_answerQuestion),
+            ...(questions[_questionIndex]['answers'] as List<String>).map((answer) {
+              return Answer(_answerQuestion, answer);
+            }).toList()
           ],
         ),
       ),
     );
   }
 }
-
