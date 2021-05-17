ae5df9e7e2d40b9723d3b264be0ce895333c1f38
Duncan Moyo <duncanfmoyo@gmail.com>
2021-05-18 00:37:42 +0200

    Getters and else-if

--- lib/result.dart
+++ lib/result.dart
@@ -1,10 +1,32 @@
 import 'package:flutter/material.dart';
 
 class Result extends StatelessWidget {
+  final int resultScore;
+
+  Result(this.resultScore);
+
+  String get resultPhrase {
+    String resultText;
+      if(resultScore <= 9) {
+        resultText = 'You scored lower than 9!';
+      } else if (resultScore <= 15) {
+        resultText = 'You scored lower than 15!';
+      } else if (resultScore <= 24 ) {
+        resultText = 'You scored lower than 24!';
+      } else {
+        resultText = 'You scored above everyone!';
+      }
+    return resultText;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text('You did it!!'),
+      child: Text(
+        resultPhrase,
+        style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+        textAlign: TextAlign.center,
+      ),
     );
   }
 }
